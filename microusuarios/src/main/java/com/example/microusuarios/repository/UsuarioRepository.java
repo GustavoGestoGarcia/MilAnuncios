824e398c0def07c2dbb679731814d4fb9f645bc8
GustavoGestoGarcia <gestogarciagustavo@gmail.com>
2023-10-09 17:24:06 +0200

    Delete microusuarios directory

--- microusuarios/src/main/java/com/example/microusuarios/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.microusuarios.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.microusuarios.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, String>{
-
-}
